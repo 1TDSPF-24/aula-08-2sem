4d602f7161a432e416eea2db9c459eec8f976067
nathan <nathan.consolo17@gmail.com>
2024-09-02 09:49:56 -0300

    Aprendendo iteracao de listas ts

---
+++ aula08/src/lista.ts
@@ -0,0 +1,39 @@
+export const listaProduto = [
+
+    {
+        id: 1,
+        nome: "Smartphone",
+        preco: 1500.00,
+        marca: "Apple",
+        categoria: "Celulares",
+        descricao: "Celular com tela de 6 polegadas e 4GB de RAM",
+        imagem: "../../public/img/produto-640x320.png",
+    },
+    {
+        id: 2,
+        nome: "Notebook",
+        preco: 3000.00,
+        marca: "Dell",
+        categoria: "Computadores",
+        descricao: "Notebook com processador Intel Core i7 e 16GB de RAM",
+        imagem: "../../public/img/produto-640x320.png",
+    },
+    {
+        id: 3,
+        nome: "Tablet",
+        preco: 800.00,
+        marca: "Samsung",
+        categoria: "Celulares",
+        descricao: "Tablet com tela de 10 polegadas e 3GB de RAM",
+        imagem: "../../public/img/produto-640x320.png",
+    },
+    {
+        id: 4,
+        nome: "Impressora",
+        preco: 200.00,
+        marca: "HP",
+        categoria: "Impressoras",
+        descricao: "Impressora de alta qualidade com tecnologia de impressão a jato de tinta",
+        imagem: "../../public/img/produto-640x320.png",
+    }
+];
--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,13 +1,78 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { listaProduto } from "../../lista";
 
+const StyledImgProdutos = styled.img`
+  display: block;
+  max-width: 100%;
+  width: 20%;
+  margin: 0 auto;
+`
 
-export default function Produtos(){
+export default function Produtos() {
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "PRODUTOS";
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "PRODUTOS";
 
-    return(
-      <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Lista de Produtos!</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Nome
+            </th>
+            <th>
+              Preço
+            </th>
+            <th>
+              Marca
+            </th>
+            <th>
+              Categoria
+            </th>
+            <th>
+              Descrição
+            </th>
+            <th>
+              Imagem
+            </th>
+            <th>
+              Editar | Excluir
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaProduto.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.marca}</td>
+              <td>{produto.descricao}</td>
+              <td><StyledImgProdutos src={produto.imagem} alt="Imagem do produto " /></td>
+              <td><Link to={`/editar/produtos/${produto.id}`}> </Link></td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            {/* colSpan é para juntar a quantidade de celulares */}
+            <td colSpan={7}>
+              Total de registros: <span>{listaProduto.length}</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
+
+// id: 1,
+// nome: "Smartphone",
+// preco: 1500.00,
+// marca: "Apple",
+// categoria: "Celulares",
+// descricao: "Celular com tela de 6 polegadas e 4GB de RAM",
+// imagem:"/img/produtos-640×462.png",
