4e87281bdda4397c072006b9d0a0cf3102d2a14a
laviniapark <laviniasoo03@gmail.com>
2024-09-02 09:45:39 -0300

    Aula 02092024

--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,13 +1,59 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { listaProdutos } from "../../listaProdutos";
 
+// css para as imagens da caixa
+const ImgProdutos = styled.img`
+  display:block;
+  max-width:100%;
+  width:20%;
+  margin: 0 auto;
+  /* o 0 não mexe com a altura */
+`
 
-export default function Produtos(){
+export default function Produtos() {
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "PRODUTOS";
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "PRODUTOS";
 
-    return(
-      <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Lista de Produtos</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Marca</th>
+            <th>Categoria</th>
+            <th>Descrição</th>
+            <th>Foto</th>
+            <th>Editar | Excluir</th>
+            {/* id não é colocado pois não vai ser mostrado para o usuário */}
+          </tr>
+        </thead>
+        <tbody>
+          {listaProdutos.map((produto) => (
+            <tr key={produto.id}> {/*o programa irá gerar as linhas baseado na qtd de registros*/}
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.marca}</td>
+              <td>{produto.categoria}</td>
+              <td>{produto.descricao}</td>
+              <td><ImgProdutos src={produto.imagem} alt={produto.nome} /></td>
+              <td>
+                <Link to={`/editar/produtos/${produto.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          {/* todas as células serão mescladas */}
+          <td colSpan={7}>
+            Total de registros : <span>{listaProdutos.length}</span>
+          </td>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
