7a4a728c37e7e5b4a70db3df5c2b079bcf0291f7
leoc7sar <lcrn1102@gmail.com>
2024-09-02 09:48:56 -0300

    Commit leoc7sar.

--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,13 +1,57 @@
 
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { listaProdutos } from "../../listaProdutos";
 
-export default function Produtos(){
+const ImgProdutos = styled.img`
+   display:block;
+   max-width:100%;
+   width:20%;
+   margin:0 auto;
+`
+export default function Produtos() {
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "PRODUTOS";
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "PRODUTOS";
 
-    return(
-      <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Lista de produtos!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Marca</th>
+            <th>Categoria</th>
+            <th>Descrição</th>
+            <th>Foto</th>
+            <th>Editar | Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaProdutos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.marca}</td>
+              <td>{produto.categoria}</td>
+              <td>{produto.descricao}</td>
+              <td><ImgProdutos src={produto.imagem} alt={produto.nome} /></td>
+              <td>
+                <Link to={`/editar/produtos/${produto.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={7}>
+              Total de registros : <span>{listaProdutos.length}</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
